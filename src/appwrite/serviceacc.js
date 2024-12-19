cebb6eaa4d6c08f0b62d350b07739b5bb0fc04d4
Pratik <pratikgudape9825@gmail.com>
2024-12-19 19:52:21 +0530

    changes in addMemberToTeam method

--- src/appwrite/serviceacc.js
+++ src/appwrite/serviceacc.js
@@ -1,5 +1,5 @@
 import Config from '../conf/conf'
-import {Client,Teams} from 'node-appwrite'
+import {Client,Teams, Users} from 'node-appwrite'
 
 
 // create a client for the team creator(team owner)
@@ -11,11 +11,14 @@ const client = new Client()
 
 // create a teams object
 const teams = new Teams(client)
-
+const users = new Users(client);
 
 // create a function which takes useid, name of the user and email of the new seller to be added tp the seller team
 const addMemberToTeam = async ({userId,userName,userEmail}) => {
     try{
+        // verify the user (email verification)
+        await users.updateEmailVerification(userId,true)
+        
         const response = await teams.createMembership(
             Config.appwritesellerTeamId,
             ["Seller"],
